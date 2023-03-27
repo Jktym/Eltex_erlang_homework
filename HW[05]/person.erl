e029f7c96474fd456444880d4d4dfd18d3f7bb1b
Jktym <yorkdan@bk.ru>
2023-03-27 11:33:14 +0700

    Commit

--- HW[05]/person.erl
+++ HW[05]/person.erl
@@ -16,6 +16,4 @@ get_average_age({_AgeSum, PersonsCount}) when PersonsCount =:= 0 ->
 get_average_age({AgeSum, PersonsCount})->
   {AgeSum, PersonsCount} = lists:foldl(fun() -> AgeSum/PersonsCount end, {0,0}).
 
-
-
 %Persons = [#person{id = 1, name = "Bob", age = 23, gender = male},#person{id = 2, name = "Kate", age = 20, gender = female}, #person{id = 3, name = "jack", age = 34, gender = male}, #person{id = 4, name = "Nata", age = 54, gender = female}].
